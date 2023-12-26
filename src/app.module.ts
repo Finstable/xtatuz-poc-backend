c0529bcdb099847c443337b4008d14b72efa8b79
Nichakan <nichakan.tanang999@gmail.com>
2023-12-26 14:21:44 +0700

    🐳 chore: set up project

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './module/users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from './config/configuration';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => config.get('database') || {},
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+      expandVariables: true,
+      envFilePath: ['.env'],
+      load: [configuration],
+    }),
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
