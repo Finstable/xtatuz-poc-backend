646d6f2ef80517f672bbe117d1a216896a95b183
Nichakan <nichakan.tanang999@gmail.com>
2023-12-28 11:40:54 +0700

    🦄 refactor: remove import

--- src/module/auth/auth.controller.ts
+++ src/module/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 
