68f36a43f2667be8cac79c6043ede34afbee4012
Nichakan <nichakan.tanang999@gmail.com>
2024-01-03 14:06:29 +0700

    ✨ feat: event log and history event log

--- src/module/properties/dto/create-property.dto.ts
+++ src/module/properties/dto/create-property.dto.ts
@@ -1,1 +1,19 @@
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
 export class CreatePropertyDto {}
+export class EventLogDto {
+  @IsOptional()
+  @IsString()
+  merchantAddress: string;
+}
+
+export class HistoryEventLogDto {
+  @IsOptional()
+  merchantAddress: string;
+
+  @IsOptional()
+  fromBlock: number;
+
+  @IsOptional()
+  toBlock: number;
+}
